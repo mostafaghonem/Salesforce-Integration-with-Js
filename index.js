0226f4df77ae589d04ba4c2e44e9c95ebfebaab7
Mostafa Ghonem <mostafaghonem90@yahoo.com>
2022-11-01 14:45:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ async function Auth() {
         ),
         params = {
             grant_type: "password",
-            client_id: "3MVG9ZlTu2QmDfb3MJF3cxRjO8gABNd3BsfEQDzzAwTbRlP_bJHFRBl_Em8MSHJOPEVQEKUMnic4oAAwl8Iam",
-            client_secret: "67B2326366A3E4D19D54C50DB718F6C014706A5B03A1289570DC62B959CF7976",
-            username: "mostafaghonem90@gmail.com",
-            password: "018410961400Mkg$$@@GHa3v0xVSUbR1SN77rCpzPsW",
+            client_id: "Your Client Id",
+            client_secret: "Your Client Sceret",
+            username: "UserName",
+            password: "PasswordSecurityToken",
         };
     Object.keys(params).forEach((key) =>
         AuthURL.searchParams.append(key, params[key])
@@ -23,7 +23,7 @@ async function CreateAcc() {
     let access_token = response.data.access_token;
     return await axios({
         method: "post",
-        url: "https://mostafaghonem-dev-ed.my.salesforce.com/services/data/v56.0/sobjects/Account/",
+        url: "https://YourDomain.my.salesforce.com/services/data/v56.0/sobjects/Account/",
         headers: {
             Authorization: `Bearer ${access_token}`,
         },
