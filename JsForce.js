cf89edc76613ef8289594c3d4c3a7f82d6756a85
mostafaghonem <mostafaghonem90@yahoo.com>
2022-11-25 17:07:48 +0200

    jsForce Update

--- JsForce.js
+++ JsForce.js
@@ -10,21 +10,21 @@ conn.login(username, password, function(err, res) {
     }
     console.log("Authenticated");
     //=============== Query====================================
-    // conn.query("SELECT Id, Name FROM Account", function(err, res) {
-    //     if (err) {
-    //         return console.error(err);
-    //     }
-    //     console.log(res);
-    // });
+    conn.query("SELECT Id, Name FROM Account", function(err, res) {
+        if (err) {
+            return console.error(err);
+        }
+        console.log(res);
+    });
 
     //=============== Streaming API pushTopic ==================
-    // conn.streaming.topic("AccountUpdate").subscribe(function(message) {
-    //     console.log("Event Type : " + message.event.type);
-    //     console.log("Event Created : " + message.event.createdDate);
-    //     console.log("Object Id : " + message.sobject.Id);
-    //     console.log("Record : " + JSON.stringify(message.sobject)); //here used stringify to see the result as string
-    //     console.log("Event : " + JSON.stringify(message));
-    // });
+    conn.streaming.topic("AccountUpdate").subscribe(function(message) {
+        console.log("Event Type : " + message.event.type);
+        console.log("Event Created : " + message.event.createdDate);
+        console.log("Object Id : " + message.sobject.Id);
+        console.log("Record : " + JSON.stringify(message.sobject)); //here used stringify to see the result as string
+        console.log("Event : " + JSON.stringify(message));
+    });
 
     // ===========================================================
     //Subscribe to a platform event
